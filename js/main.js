de08d04bdf4f1c430da8b3497a995b7a6fa32a30
Joclark <joclark2009@live.co.uk>
2016-01-06 21:33:03 +0000

    update just before handing in
    Done javascript and only need some minor changes.

--- js/main.js
+++ js/main.js
@@ -2,39 +2,41 @@
 console.log('it works');
 $(document).ready(function(){
 
-   	$(firstOne).show()
-	$('#primary-nav .fire-nav').on('click', function(event){
+   	// $(firstOne).show()
+	// $('#primary-nav .fire-nav').on('click', function(event){
 
-		event.preventDefault();
-		$('.drop').fadeOut(200)
+		// event.preventDefault();
+		// $('.drop').fadeOut(200)
 
-		var idToShow = $(this).attr('href');
+		// var idToShow = $(this).attr('href');
 
-		$(idToShow).slideDown()
+		// $(idToShow).slideDown()
+		// wills stuff?
+
 
 		// if I click international again then
-		if ($('#primary-nav .find-a-trainer').hasClass('active')) {
+		// if ($('#primary-nav .find-a-trainer').hasClass('active')) {
 			// remove the active class
-			$('#primary-nav .find-a-trainer').removeClass('active');
+			// $('#primary-nav .find-a-trainer').removeClass('active');
 			// slide the mega nav up
-			$('#slide-down').slideUp();
-		} else {
+			// $('#slide-down').slideUp();
+		// } else {
 			// add an active class to the item
-			$('#primary-nav li.find-a-trainer').addClass('active');
+			// $('#primary-nav li.find-a-trainer').addClass('active');
 			// hide all the sections
-			$('.drop').hide();
+			// $('.drop').hide();
 			// show the international section
-			$('#slide-down .find-a-trainer').show();
+			// $('#slide-down .find-a-trainer').show();
 			// the mega nav slides down
-			$('#slide-down').slideDown();
-		}
-	});
-// when I click on the international list item
+			// $('#slide-down').slideDown();
+		// }
+	// });
+// when I click on the find a trainer list item
 $('#primary-nav .find-a-trainer').on('click', function(event){
 
 	event.preventDefault();
 
-	// if I click international again then
+	// if I click find a trainer again then
 	if ($('#primary-nav .find-a-trainer').hasClass('active')) {
 		// remove the active class
 		$('#primary-nav .find-a-trainer').removeClass('active');
@@ -44,8 +46,8 @@ $('#primary-nav .find-a-trainer').on('click', function(event){
 		// add an active class to the item
 		$('#primary-nav li.find-a-trainer').addClass('active');
 		// hide all the sections
-		$('.drop').hide();
-		// show the international section
+		$('.drop').show();
+		// show the find a trainer section
 		$('#slide-down .find-a-trainer').show();
 		// the mega nav slides down
 		$('#slide-down').slideDown();
@@ -61,7 +63,7 @@ $('#primary-nav .find-a-trainer').on('click', function(event){
 		// remove the active class
 		$('#primary-nav .about-us').removeClass('active');
 		// slide the mega nav up
-		$('#slide-down').slideUp();
+		$('#slide-down2').slideUp();
 	} else {
 		// add an active class to the item
 		$('#primary-nav li .about-us').removeClass('active');
@@ -69,32 +71,101 @@ $('#primary-nav .find-a-trainer').on('click', function(event){
 		// hide all the sections
 		$('.drop').hide();
 		// show the politics section
-		$('#slide-down .about-us').show();
+		$('#slide-down2 .about-us').show();
 		// the mega nav slides down
-		$('#slide-down').slideDown();
+		$('#slide-down2').slideDown();
 		}
 	});
 
-	$('primary-nav .leadership-and-personal-development').on('click', function(event){
+	$('#primary-nav .leadership-and-personal-development').on('click', function(event){
 
 	event.preventDefault();
 
 	// if I click international again then
-	if ($('primary-nav.leadership-and-personal-development').hasClass('active')) {
+	if ($('#primary-nav.leadership-and-personal-development').hasClass('active')) {
 		// remove the active class
-		$('primary-nav .leadership-and-personal-development').removeClass('active');
+		$('#primary-nav .leadership-and-personal-development').removeClass('active');
 		// slide the mega nav up
-		$('#slide-down').slideUp();
+		$('#slide-down3').slideUp();
 	} else {
 		// add an active class to the item
-		$('primary-nav li .leadership-and-personal-development').removeClass('active');
-		$('.nav .leadership-and-personal-development').addClass('active');
+		$('#primary-nav li .leadership-and-personal-development').removeClass('active');
+		$('#primary-nav .leadership-and-personal-development').addClass('active');
 		// hide all the sections
 		$('.drop').hide();
 		// show the politics section
-		$('#slide-down .leadership-and-personal-development').show();
+		$('#slide-down3 .leadership-and-personal-development').show();
 		// the mega nav slides down
-		$('#slide-down').slideDown();
+		$('#slide-down3').slideDown();
+		}
+	});
+
+	$('#primary-nav .it-training').on('click', function(event){
+
+	event.preventDefault();
+
+	// if I click international again then
+	if ($('#primary-nav.it-training').hasClass('active')) {
+		// remove the active class
+		$('#primary-nav .it-training').removeClass('active');
+		// slide the mega nav up
+		$('#slide-down4').slideUp();
+	} else {
+		// add an active class to the item
+		$('#primary-nav li .it-training').removeClass('active');
+		$('#primary-nav .it-training').addClass('active');
+		// hide all the sections
+		$('.drop').hide();
+		// show the politics section
+		$('#slide-down4 .it-training').show();
+		// the mega nav slides down
+		$('#slide-down4').slideDown();
+		}
+	});
+
+$('#primary-nav .trainers-profiles').on('click', function(event){
+
+	event.preventDefault();
+
+	// if I click international again then
+	if ($('#primary-nav .trainers-profiles').hasClass('active')) {
+		// remove the active class
+		$('#primary-nav .trainers-profiles').removeClass('active');
+		// slide the mega nav up
+		$('#slide-down5').slideUp();
+	} else {
+		// add an active class to the item
+		$('#primary-nav li .trainers-profiles').removeClass('active');
+		$('#primary-nav .trainers-profiles').addClass('active');
+		// hide all the sections
+		$('.drop').hide();
+		// show the politics section
+		$('#slide-down5 .trainers-profiles').show();
+		// the mega nav slides down
+		$('#slide-down5').slideDown();
+		}
+	});
+
+$('#primary-nav .trainer-jobs').on('click', function(event){
+
+	event.preventDefault();
+
+	// if I click international again then
+	if ($('#primary-nav .trainer-jobs').hasClass('active')) {
+		// remove the active class
+		$('#primary-nav .trainer-jobs').removeClass('active');
+		// slide the mega nav up
+		$('#slide-down6').slideUp();
+	} else {
+		// add an active class to the item
+		$('#primary-nav li .trainer-jobs').removeClass('active');
+		$('#primary-nav .trainer-jobs').addClass('active');
+		// hide all the sections
+		$('.drop').hide();
+		// show the politics section
+		$('#slide-down6 .trainer-jobs').show();
+		// the mega nav slides down
+		$('#slide-down6').slideDown();
 		}
 	});
 
